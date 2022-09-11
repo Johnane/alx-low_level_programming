e722e2c8dab5f361b25ca869e844485fd6bffce0
Johnane <johnnny.john.5.4@gmail.com>
2022-09-10 23:30:44 -0700

    unix is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - unix is simple
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1)
+}
