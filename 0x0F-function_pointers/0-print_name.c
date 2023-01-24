04319e539b9b404b4e53f2eb26d3c9483cdd2da1
Johnane <johnnny.john.5.4@gmail.com>
2023-01-24 02:35:43 -0800

    new versions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints name
  * @name: Name to print
  * @f: pointer variable to print from
- */
+ * Return: nothing
+ **/
 
 void print_name(char *name, void (*f)(char *))
 {
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * array_iterator - Calls a function to work on an array
  * @array: array to be used
- * @size_t: size of the array
+ * @size: size of the array
  * @action: function to be called
  * Return: The result after function call
  */
