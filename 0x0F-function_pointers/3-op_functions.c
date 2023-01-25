e880493769542628c0fc116003fa654068de74c8
Johnane <johnnny.john.5.4@gmail.com>
2023-01-25 14:58:34 -0800

    ..

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds thr value of two integers
+ * @a: First int to add
+ * @b: Second int to add
+ * Return: The sum of the two ints
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b):
+}
+
+/**
+ * op_sub - subtract the value of two integers
+ * @a: First int to add
+ * @b: Second int to add
+ * Return: The difference of the two ints
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b):
+}
+
+/**
+ * op_mul - multiplies the value of two integers
+ * @a: First int to add
+ * @b: Second int to add
+ * Return: The product of the two ints
+ */
+
+int op_mul(int a , int b)
+{
+        return (a * b):
+}
+
+/**
+ * op_add - the quotient the value of two integers
+ * @a: int to divide
+ * @b: int to divide by
+ * Return: The of the two ints
+ */
+
+int op_div(int a, int b)
+{
+	if (b = 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+        return (a / b):
+}
+
+
+/**
+ * op_add - The remainder of the value of two integers
+ * @a: First int to add
+ * @b: Second int to add
+ * Return: The sum of the two ints
+ */
+
+int op_add(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+        return (a % b):
+}
