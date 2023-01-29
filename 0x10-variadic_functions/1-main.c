3969edf5356444d795ecc34f7ad2d413b0574dad
Johnane <johnnny.john.5.4@gmail.com>
2023-01-29 03:50:21 -0800

    today

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int main(void)
+{
+    print_numbers(", ", 3, 1, 2, 3);
+    return 0;
+}
+
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,12 +20,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(args, int));
-			if (i < n - 1 && seperator != NULL)
-				printf("%s",seperator);
+			if (i < n - 1 && separator != NULL)
+				printf("%s",separator);
 		}
 		va_end(args);
 	}
 	printf("\n");
 }
 
+int main(void)
+{
+    print_numbers(", ", 3, 1, 2, 3);
+    return 0;
+}
 
